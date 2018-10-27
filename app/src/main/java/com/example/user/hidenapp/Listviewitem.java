774d31199a2491ce3e6c312bf3c87f359a00d219
김진재 <killyou000703@gmail.com>
2018-10-27 15:26:37 +0900

    End

---
+++ app/src/main/java/com/example/user/hidenapp/Listviewitem.java
@@ -0,0 +1,17 @@
+package com.example.user.hidenapp;
+
+public class Listviewitem {
+    private int icon;
+    private String name;
+
+    public int getIcon(){
+        return icon;
+    }
+    public String getName(){
+        return name;
+    }
+    public Listviewitem(int icon,String name){
+        this.icon=icon;
+        this.name=name;
+     }
+}
--- app/src/main/java/com/example/user/hidenapp/MainActivity.java
+++ app/src/main/java/com/example/user/hidenapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.user.hidenapp;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -16,6 +19,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import java.text.Collator;
 import java.util.ArrayList;
@@ -24,39 +28,36 @@ import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    List<ApplicationInfo> mPackageApps = new ArrayList<ApplicationInfo>();
+    private ListView mListView;
 
-    EditText editText = (EditText)findViewById(R.id.editText);
-    Button button = (Button)findViewById(R.id.button);
-    String  appName;
-    Context context;
-
-    @SuppressLint("LongLogTag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PackageManager pkgm = context.getPackageManager();
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> AppInfos = pkgm.queryIntentActivities(intent, 0);
-        for (ResolveInfo info : AppInfos) {
-            ActivityInfo ai = info.activityInfo;
-            Log.i("GUNMAN: APP TITLE", ai.loadLabel(pkgm).toString());
-            Log.i("GUNMAN: APP Package Name", ai.packageName);
-            Log.i("GUNMAN: APP Class Name", ai.name);
-            int resId = ai.applicationInfo.icon; //App. Icon
+        mListView = findViewById(R.id.list_item);
+
+        dataSetting();
+    }
+
+    private void dataSetting(){
+
+        MyAdapter mMyAdapter = new MyAdapter();
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> pkgAppsList = getApplicationContext().getPackageManager().queryIntentActivities( mainIntent, 0);
+        for(int i = 0; i < pkgAppsList.size(); i++) {
+            Log.i("img", "dataSetting: " + pkgAppsList.get(i));
         }
 
-        button.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                appName = editText.getText().toString();
+        for (int i = 0; i < pkgAppsList.size(); i++) {
+            mMyAdapter.addItem(pkgAppsList.get(i).activityInfo.loadIcon(getPackageManager()), pkgAppsList.get(i).activityInfo.loadLabel(getPackageManager()).toString(), pkgAppsList.get(i).activityInfo.name);
+          //  mMyAdapter.addItem(pkgAppsList.get(i).activityInfo.getIconResource(), pkgAppsList.get(i).activityInfo.loadLabel(getPackageManager()).toString(), "순서"+i+1);
+        }
 
-            }
-        });
+        /* 리스트뷰에 어댑터 등록 */
+        mListView.setAdapter(mMyAdapter);
+
     }
 }
-
-
